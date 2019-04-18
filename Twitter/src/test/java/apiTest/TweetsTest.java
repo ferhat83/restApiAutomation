3031b4ab708289849577113e85c5e918997bc980
ferhat83 <saadiferhatsaid@gmail.com>
2019-04-18 18:32:57 -0400

    after method

--- Twitter/src/test/java/apiTest/TweetsTest.java
+++ Twitter/src/test/java/apiTest/TweetsTest.java
@@ -3,15 +3,19 @@ package apiTest;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import tweets.TweetsUser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class TweetsTest {
     protected TweetsUser tweetsUser;
     protected Long tweetId;
+    protected List<Long> tweetIdList= new ArrayList<Long>();
 
     @BeforeClass
     public void setUp(){
@@ -29,6 +33,7 @@ public class TweetsTest {
         response.then().statusCode(HttpStatus.SC_OK);
         Assert.assertEquals(200,response.getStatusCode());
         this.tweetId = response.path("id");
+        this.tweetIdList.add(this.tweetId);
     }
    @Test
     public void cantNotTweetWithSameTweetTwiceInRow()throws Exception{
@@ -36,15 +41,25 @@ public class TweetsTest {
         Response response = tweetsUser.createTweet(tweet);
         response.then().statusCode(HttpStatus.SC_OK);
         Assert.assertEquals(200,response.getStatusCode());
+       this.tweetIdList.add(this.tweetId);
 
         response = tweetsUser.createTweet(tweet);
         response.then().statusCode(HttpStatus.SC_FORBIDDEN);
         Assert.assertEquals(403,response.getStatusCode());
+
     }
-   @Test(dependsOnMethods = {"testCreateTweet"})
+   @Test(dependsOnMethods = {"testCreateTweet"},enabled = false)
     public void testUserCanDeleteTweet(){
         Response response = this.tweetsUser.deleteTweet(this.tweetId);
         response.then().statusCode(HttpStatus.SC_OK);
         Assert.assertEquals(200,response.getStatusCode());
+    }
+    @AfterClass
+    public void cleanUp(){
+        if(this.tweetIdList!= null){
+            for(Long tweetId: tweetIdList){
+                this.tweetsUser.deleteTweet(tweetId);
+            }
+        }
     }
 }
